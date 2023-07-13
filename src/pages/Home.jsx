5b147e2e81871aacca10322f23246fa16a0373c1
huzaifa1993 <huothyfayusuf1993@gmail.com>
2023-07-13 16:29:12 +0100

    create repo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../component/Veggie";
+import Popular from "../component/Popular";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  );
+}
+
+export default Home;
